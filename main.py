c782c4da53b83e089130a404ce8dca9c19f4cf96
Rafael Silva <rafael.silva.06@outlook.com>
2024-07-07 18:49:47 -0600

    Se creo la función y se modifico para que utilice variables

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 calcultation_to_seconds = 24 
 name_of_unit= "hours"
 
-def days_to_units():
-    print(f"20 days are {20 * calcultation_to_seconds} {name_of_unit}")
+def days_to_units(num_of_days):
+    print(f"{num_of_days} days are {num_of_days * calcultation_to_seconds} {name_of_unit}")
     print("All godd!")
 
-print(f"35 days are {35 * calcultation_to_seconds} {name_of_unit}")
-print(f"50 days are {50 * calcultation_to_seconds} {name_of_unit}")
-print(f"110 days are {110 * calcultation_to_seconds} {name_of_unit}")
+days_to_units(35)
