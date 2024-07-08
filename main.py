e9c8c0e8c02cfb25b48b9ac2dc50719391ec0759
Rafael Silva <rafael.silva.06@outlook.com>
2024-07-07 18:57:05 -0600

    Descripción de los cambios realizados

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 calcultation_to_seconds = 24 
 name_of_unit= "hours"
 
-def days_to_units(num_of_days):
+def days_to_units(num_of_days, custom_message):
     print(f"{num_of_days} days are {num_of_days * calcultation_to_seconds} {name_of_unit}")
-    print("All godd!")
+    print(custom_message)
 
-days_to_units(35)
+def scope_check(num_of_days):
+    my_var = "variable inside function"
+    print(name_of_unit)
+    print(num_of_days)
+    print(my_var)
+
+
+scope_check(20)
