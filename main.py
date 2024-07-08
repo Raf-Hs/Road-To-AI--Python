5779e190f22c14009ed5cc33e750f0795b81f8cb
Rafael Silva <rafael.silva.06@outlook.com>
2024-07-07 19:35:12 -0600

    Validations were added.

--- main.py
+++ main.py
@@ -2,10 +2,21 @@ calcultation_to_seconds = 24
 name_of_unit= "hours"
 
 def days_to_units(num_of_days):
-    return f"{num_of_days} days are {num_of_days * calcultation_to_seconds} {name_of_unit}"
+    condition_check = num_of_days > 0
+    print (type(condition_check))
 
-my_var = days_to_units(20)
-print(my_var)
+    if num_of_days > 0:
+        return f"{num_of_days} days are {num_of_days * calcultation_to_seconds} {name_of_unit}"
+    elif num_of_days == 0:
+        return("The number is zero, so no conversion for you")
+    else: 
+        return("The number is negative, so no conversion for you")
 
 user_input = input("Hey user, enter a number of days and I will convert it to hours!\n")
-print(user_input)
+
+if user_input.isdigit():
+    calculated_value = days_to_units(int(user_input))
+    print(calculated_value)
+else:
+    print("Your input is not a number")
+    
