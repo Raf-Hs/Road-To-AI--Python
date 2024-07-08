1b2bd4394a7bd06ddc6af33f3a4dc8a553d2b8fd
Rafael Silva <rafael.silva.06@outlook.com>
2024-07-07 18:44:42 -0600

    Ejericios Basicos de Python

---
+++ main.py
@@ -0,0 +1,10 @@
+calcultation_to_seconds = 24 
+name_of_unit= "hours"
+
+def days_to_units():
+    print(f"20 days are {20 * calcultation_to_seconds} {name_of_unit}")
+    print("All godd!")
+
+print(f"35 days are {35 * calcultation_to_seconds} {name_of_unit}")
+print(f"50 days are {50 * calcultation_to_seconds} {name_of_unit}")
+print(f"110 days are {110 * calcultation_to_seconds} {name_of_unit}")
